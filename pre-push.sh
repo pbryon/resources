e17bcc77220f90977985d9bd716094223f38078b
Pieter Bryon <bryonpieter@gmail.com>
2019-09-16 14:53:47 +0200

    Revert "Tweak pre-push hook"
    This reverts commit bf1d1a262d75a8f0f9616709630f2c992857f4e3.

--- pre-push.sh
+++ pre-push.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-echo Link checker - pre-push hook
-./test.sh --minimal --debug
+files=$(git diff-index --cached --name-only --diff-filter=ACMR HEAD | grep topics)
+error=0
+if [ ${#files} -eq 0 ]; then
+    exit 0
+fi
+
+echo Link checker - pre-commit hook
+./test.sh $files --minimal --debug
 if [ $? -ne 0 ]; then
     exit 1
 else
