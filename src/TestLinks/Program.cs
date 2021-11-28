5a7545929100b952ea0a71a97b670bc52b7712e6
Pieter Bryon <bryonpieter@gmail.com>
2021-11-28 16:13:04 +0100

    Convert to top-level program

--- src/TestLinks/Program.cs
+++ src/TestLinks/Program.cs
@@ -1,19 +1,11 @@
 using System;
 using System.Threading;
-using System.Threading.Tasks;
+using TestLinks;
 using TestLinks.Extensions;
 
-namespace TestLinks
-{
-    internal class Program
-    {
-        private static async Task Main(string[] args)
-        {
-            args = args.GetLogLevel(out var logLevel);
-            var hadError = await new TopicValidator(logLevel)
-                .Validate(new CancellationTokenSource().Token, args);
+args = args.GetLogLevel(out var logLevel);
+var hadError =
+    await new TopicValidator(logLevel)
+    .Validate(new CancellationTokenSource().Token, args);
 
-            Environment.Exit(hadError ? 1 : 0);
-        }
-    }
-}
+Environment.Exit(hadError ? 1 : 0);
