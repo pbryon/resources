a8811cfb40b514dc22038b0a9362f3873eff89b3
Pieter Bryon <bryonpieter@gmail.com>
2021-11-28 16:02:40 +0100

    Revert "Use Firefox user agent"
    This reverts commit f27969ec09a55d11b3027c4562d276b05dcaa6a1.

--- src/TestLinks/Extensions/HttpClientExtensions.cs
+++ src/TestLinks/Extensions/HttpClientExtensions.cs
@@ -9,7 +9,7 @@ namespace TestLinks.Extensions
         {
             client.Timeout = TimeSpan.FromMinutes(1);
             client.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "text/html");
-            client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/94.0");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "script");
 
             return client;
         }
